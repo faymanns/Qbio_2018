ee5ebc458aa998cbd8146fc0df0b1b34b7925311
faymanns <florian.aymanns@epfl.ch>
2018-08-13 21:31:50 -0700

    separate lanes and save avi

--- step_1.py
+++ step_1.py
@@ -1,23 +1,77 @@
 import os.path
+from subprocess import call
 import numpy as np
 import cv2
 from skimage import io
 
-#path = os.path.expanduser(os.path.expandvars('~/Desktop/R38B08R81E10_0_5mW_1_1.tif'))
+def hysteresis_filter(seq, n=5):
+    """
+    This function implements a hysteresis filter for boolean sequences.
+    The state in the sequence only changes if n consecutive element are in a different state.
+
+    Parameters
+    ----------
+    seq : 1D np.array of type boolean
+        Sequence to be filtered.
+    n : int, default=5
+        Length of hysteresis memory.
+
+    Returns
+    -------
+    seq : 1D np.array of type boolean
+        Filtered sequence.
+    """
+    seq = seq.astype(np.bool)
+    state = seq[0]
+    start_of_state = 0
+    memory = 0
+    for i in range(len(seq)):
+        if state != seq[i]:
+            memory += 1
+        elif memory < n:
+            memory = 0
+            continue
+        if memory == n:
+            seq[start_of_state:i-n+1]=state
+            start_of_state = i-n+1
+            state = not state
+            memory = 0
+    seq[start_of_state:]=state
+    return seq
+
 path = os.path.expanduser(os.path.expandvars('~/Desktop/sample_input.tif'))
 full_video = io.imread(path)
-#print(full_video.dtype)
-# print(len(full_video))
 
 average_image = np.zeros_like(full_video[0])
 
 for i,frame in enumerate(full_video):
     #thresh = cv2.threshold(frame, 0, 65535, cv2.THRESH_OTSU)
     thresh = cv2.threshold(frame, 25000, 65535, cv2.THRESH_BINARY)
-    #print(thresh[0])
     #io.imsave(f'thresh{i}.tif', thresh[1])
     average_image += thresh[1]
 
 average_image = average_image / len(full_video)
-print(average_image)
-io.imsave('result.tif', average_image.astype(np.uint16))
+#io.imsave('result.tif', average_image.astype(np.uint16))
+
+bool_rows = np.any(average_image, axis=1)
+bool_rows = hysteresis_filter(bool_rows, 4)
+
+lanes = []
+new_lane = True
+for i,b in enumerate(bool_rows):
+    if new_lane and b:
+        lanes.append(i-1)
+        new_lane = False
+    elif not new_lane and not b:
+        lanes.append(i)
+        new_lane = True
+print(lanes)
+for i in range(0, len(lanes)-1, 2):
+    lane_id = int(i/2)
+    #io.imsave(f'lane{lane_id}.tif', full_video[:, lanes[i]:lanes[i+1], :])
+    if not os.path.exists('frames'):
+            os.makedirs('frames')
+    for j,img in enumerate(full_video[:, lanes[i]:lanes[i+1], :]):
+        io.imsave(f'frames/lane_{lane_id}_frame_{j}.tif', img)
+    call(['ffmpeg', '-y', '-i', f'frames/lane_{lane_id}_frame_%d.tif', f'lane_{lane_id}.avi'])
+    call(['rm', '-r', 'frames'])
