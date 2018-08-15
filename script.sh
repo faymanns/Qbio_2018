fe83f227f59031fbb13046d143a34ff0f28b4e31
faymanns <florian.aymanns@epfl.ch>
2018-08-15 16:14:29 -0700

    iterate over folders

--- script.sh
+++ script.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
-dir="~/Desktop/Qbio_2018"
-tif_file="${dir}/sample_input.tif"
-laser_file="${dir}/laserposition_paper.jpg"
-out_dir="."
-./step_1.py $tif_file $laser_file $out_dir
+for dir in ~/Desktop/Qbio_2018/data*
+do
+    tif_file="${dir}/sample_input.tif"
+    laser_file="${dir}/laserposition_paper.jpg"
+    out_dir="${dir}/analysis_output"
+    mkdir $out_dir
+    ./step_1.py $tif_file $laser_file $out_dir
+done
