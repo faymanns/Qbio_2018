13f4373ceb2178244194a650a44b590c4f05fa67
niragkadakia <niragkadakia@gmail.com>
2018-08-15 20:51:14 -0400

    Add ROI tracking script

---
+++ ROI_track.py
@@ -0,0 +1,206 @@
+"""
+Class objects and functions to categorize fly in to various regions of 
+interest within the walking assay.
+
+
+Created by Nirag Kadakia at 12:00 08-14-2018
+This work is licensed under the 
+Creative Commons Attribution-NonCommercial-ShareAlike 4.0 
+International License. 
+To view a copy of this license, visit 
+http://creativecommons.org/licenses/by-nc-sa/4.0/.
+"""
+
+import scipy as sp
+import matplotlib.pyplot as plt
+import json
+import argh
+import os
+
+
+class classify_ROIs(object):
+	"""
+	Classify fly as being in particular region of interest in the assay.
+	"""
+	
+	def __init__(self, mm_per_px=3./106, ROI_width=3.5, fps=60, 
+					min_ROI_sec=0.25):
+		
+		# Number of slots in assay. 3 values for L, laser, and R, respectively.
+		self.num_slots = 4
+		self.pos_arr = sp.zeros((3, self.num_slots))
+		
+		# Minimum number of frames to count as a region-to-region transition
+		self.min_ROI_frames = fps*min_ROI_sec
+		
+		# Width of near-wall zone in pixels
+		self.ROI_width = ROI_width/mm_per_px
+		
+		# Bin edges for the ROIs. There are 6 ROIs in each slot.
+		self.ROI_bins = sp.zeros((7, self.num_slots))
+		
+		# Construct data variables
+		self.num_frames = None
+		self.data = None
+		
+		# ROI nominal assignments
+		self.raw_ROI = None
+		self.corr_ROI = None
+		self.ROI_splits = None
+		
+	def get_laser_wall_pos(self, path):
+		"""
+		Set the x-position of the walls for each channel in the walking assay.
+		
+		Parameters
+		----------
+		path : str
+			Path of json file. File should be a dictionary with x keys, 
+			slot_1,..., slot_x, each of which is a dictionary containing 3 
+			keys, "right_wall", "laser", and "left_wall". The values of 
+			each of these keys are floats representing the pixels.
+		
+		"""
+		
+		with open(path, 'r') as fp:
+			pos_dict = json.load(fp)
+		
+		for iS in range(self.num_slots):
+			self.pos_arr[0, iS] = pos_dict['slot_%s' % iS]['left_wall']
+			self.pos_arr[1, iS] = pos_dict['slot_%s' % iS]['laser']
+			self.pos_arr[2, iS] = pos_dict['slot_%s' % iS]['right_wall']
+		
+		self.ROI_bins[0] = self.pos_arr[0]
+		self.ROI_bins[1] = self.pos_arr[0] + self.ROI_width
+		self.ROI_bins[2] = self.pos_arr[1] - self.ROI_width
+		self.ROI_bins[3] = self.pos_arr[1] 
+		self.ROI_bins[4] = self.pos_arr[1] + self.ROI_width
+		self.ROI_bins[5] = self.pos_arr[2] - self.ROI_width
+		self.ROI_bins[6] = self.pos_arr[2] 
+	
+	def get_centroid_data(self, path):
+		"""
+		Set the centroid x-coordiantes from file. 
+		
+		Parameters
+		----------
+		path : str
+			Path of centroid file, in txt format. File should be a 
+			N x num_slots array, with N frames, corresponding to the x-pos.
+			of the centroid for each of the slots.
+		
+		"""
+		
+		self.data = sp.loadtxt(path)
+		self.num_frames = self.data.shape[0]
+		self.raw_ROI = sp.empty((self.num_frames, self.num_slots))
+		self.corr_ROI = sp.zeros(self.data.shape)
+			
+	def ROI_nominal(self):
+		"""
+		Set the ROIs nominally from file, for each slot separately.
+		"""
+		
+		for iS in range(self.num_slots):
+			self.raw_ROI[:, iS] = sp.digitize(self.data[:, iS], 
+									self.ROI_bins[:, iS]) - 1
+			self.corr_ROI[:, iS] = self.raw_ROI[:, iS]
+	
+	def ROI_corrected(self):
+		"""
+		Correct ROI by incorporating minimum transition time
+		"""
+		
+		for iS in range(self.num_slots):
+
+			# Indices at which ROI changes
+			splits = sp.nonzero(sp.diff(self.raw_ROI[:, iS]))[0]
+			
+			# For each of these transitions, check length for false transitions
+			for iSplit in sp.arange(len(splits))[::-1]:
+				
+				# Frame-length of the current ROI
+				split_beg = splits[iSplit - 1]
+				split_end = splits[iSplit]
+				split_len =  split_end - split_beg 
+				
+				# If ROI is too short, keep moving backward until you find an 
+				# ROI of sufficient length (i.g. ROI > min_ROI_frames)
+				split_shift = 0
+				while split_len < self.min_ROI_frames:
+					split_shift += 1
+					
+					# If at beginning of array; break the loop
+					if iSplit - split_shift < 0: 
+						break
+					split_shift_beg = splits[iSplit - split_shift - 1]
+					split_shift_end = splits[iSplit - split_shift]
+					split_len = split_shift_end - split_shift_beg
+				
+				# Change all false regions back to ROI of sufficient length
+				if split_shift > 0:
+					frames_to_change = range(split_shift_end, split_end + 1)
+					self.corr_ROI[frames_to_change, iS] = \
+						self.raw_ROI[split_shift_end, iS]
+	
+	def ROI_splits(self):
+		"""
+		Get the frames corresponding to a beginning and end of an ROI.
+		"""
+		
+		self.ROI_splits = sp.zeros(self.num_slots)
+		
+		for iS in range(self.num_slots):
+			split_idxs = sp.nonzero(sp.diff(self.corr_ROI[:, iS]))[0]
+			split_idxs = sp.hstack(([-1], split_idxs))
+			if (self.num_frames - 1) not in split_idxs:
+				split_idxs = sp.hstack((split_idxs, self.num_frames - 1))
+			
+			for iI in range(len(split_idxs) - 1):
+				idx_beg = split_idxs[iI] + 1
+				idx_end = split_idxs[iI + 1] + 1
+				arr = [self.corr_ROI[idx_beg + 1, iS], idx_beg, idx_end, iS]
+				self.ROI_splits = sp.vstack((self.ROI_splits.T, arr)).T
+		self.ROI_splits = self.ROI_splits.astype(int)
+		
+	def save_data(self, out_dir):
+		"""
+		Output nominal and corrected data and ROI splits as txt files. The
+		ROI_splits file is saved by row, where each row corresponnds to 
+		a given snippet of the video in which the fly is in a unique ROI.
+		The first column is the ROI (from {0, 6} as defined above), second 
+		and third are the beg and end frame numbers of the snippet, and 
+		last column is the slot number (up to num_slots).
+		
+		Parameters
+		----------
+		out_dir : str
+			Directory of where to save data.
+		
+		"""
+		
+		nominal_output_path =  os.path.join(out_dir, 'nominal_ROIs.txt')
+		corr_output_path =  os.path.join(out_dir, 'corrected_ROIs.txt')
+		ROI_splits_path = os.path.join(out_dir, 'ROI_frame_splits.txt')
+		with open(nominal_output_path, 'w') as fp:
+			sp.savetxt(fp, self.raw_ROI, fmt='%d', delimiter='\t')
+		with open(corr_output_path, 'w') as fp:
+			sp.savetxt(fp, self.corr_ROI, fmt='%d', delimiter='\t')
+		with open(ROI_splits_path, 'w') as fp:
+			sp.savetxt(fp, self.ROI_splits.T, fmt='%d', delimiter='\t')
+		
+		
+def main(data_path, wall_locs_path, out_dir, mm_per_px=3./106, 
+			ROI_width=3.5, fps=60, min_ROI_sec=0.25):
+	
+	a = classify_ROIs(mm_per_px, ROI_width, fps, min_ROI_sec)
+	a.get_laser_wall_pos(wall_locs_path)
+	a.get_centroid_data(data_path)
+	a.ROI_nominal()
+	a.ROI_corrected()
+	a.ROI_splits()
+	a.save_data(out_dir)
+	
+	
+if __name__ == '__main__':
+	argh.dispatch_command(main)
