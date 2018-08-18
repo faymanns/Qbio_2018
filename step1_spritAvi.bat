fa38e1d991a698a829c9f6cdd6fe636e4382bdee
takuto-okuno-t5 <okuno.takuto.t5@dc.tohoku.ac.jp>
2018-08-18 15:02:48 -0700

    fixed batch file name.

--- step1_spritAvi.bat
+++ step1_spritAvi.bat
@@ -2,7 +2,7 @@ SET EXPDIR="d:\Fly videos\Experiment"
 
 for /f "tokens=*" %%G in ('dir /b /a:d %EXPDIR%\*') do (
     mkdir %EXPDIR%\%%G\analysis_output
-    if not exist %EXPDIR%\%%G\analysis_output\lane_3.avi (
+    if not exist %EXPDIR%\%%G\analysis_output\position.json (
         python step_1.py %EXPDIR%\%%G\%%G_MMStack_Pos0.ome.tif %EXPDIR%\%%G\laserposition_paper.tif %EXPDIR%\%%G\analysis_output
     )
 )
---
+++ step3_analyzeROI.bat
@@ -0,0 +1,10 @@
+SET EXPDIR="d:\Fly videos\Experiment"
+
+for /f "tokens=*" %%G in ('dir /b /a:d %EXPDIR%\*') do (
+    mkdir %EXPDIR%\%%G\analysis_output
+    if exist %EXPDIR%\%%G\analysis_output\lane_3.avi_x.txt (
+        if not exist %EXPDIR%\%%G\analysis_output\corrected_ROIs.txt (
+            python ROI_track.py %EXPDIR%\%%G\analysis_output %EXPDIR%\%%G\analysis_output
+        )
+    )
+)
